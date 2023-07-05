9d95a98db992e15e4ea2c50f06f0100c9071a0ff
Raj Dhinge <rajkumar.dhinge.mca15@gmail.com>
2023-07-06 05:06:09 +0530

    :memo: storing JWT token into localStorage

--- react-decoupled-wordpress/src/App.js
+++ react-decoupled-wordpress/src/App.js
@@ -4,10 +4,23 @@ import Login from './components/Login';
 
 function App() {
 	const auth = useAuth();
-	console.log(auth);
+	// if (auth.isAuthenticated) {
+	// 	const body = JSON.stringify({
+	// 		title: 'My New Post',
+	// 		content: 'This is the content of my post.'
+	// 	});
+	// 	auth.fetchData('http://react.local/wp-json/wp/v2/posts', 'POST', body);
+	// }
+
 	return (
 		<>
-			<Login />
+			{auth.isAuthenticated ?
+				(
+					<>
+						<button onClick={auth.logout}>logout</button><br />Welcome!
+					</>
+				) :
+				<Login />}
 		</>
 	);
 }
--- react-decoupled-wordpress/src/components/AuthProvider.js
+++ react-decoupled-wordpress/src/components/AuthProvider.js
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { BASE_URL } from "./constants.js";
 
 const AuthContext = createContext();
 
@@ -11,20 +12,36 @@ const AuthProvider = (props) => {
 	const [token, setToken] = useState(false);
 
 	const auth = {
-		isAuthenticated,
-		token,
-		fetchData: async (url) => {
+		isAuthenticated: isAuthenticated,
+		token: token,
+		fetchData: async (url, method = 'GET', body = '') => {
+
+
+			console.log(url);
+			console.log(method);
+			console.log(body);
+			console.log(auth.token);
+			console.log(auth.isAuthenticated);
+
 			const token = localStorage.getItem('token');
+
 			if (!token) {
 				console.error('No token found. Please login first.');
 				return;
 			}
 
+
+
+			console.log("1");
 			try {
+				console.log("Processing the data upload");
 				const response = await fetch(url, {
+					method: method,
 					headers: {
+						'Content-Type': 'application/json',
 						'Authorization': `Bearer ${token}`
-					}
+					},
+					body: body
 				});
 
 				if (response.ok) {
@@ -42,34 +59,54 @@ const AuthProvider = (props) => {
 			}
 		},
 		login: async (username, password) => {
-			const response = await fetch('https://react.local/wp-json/jwt-auth/v1/token', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({ username, password })
-			});
-			console.log(response);
-			if (response.ok) {
-				const data = await response.json();
-				const curr_token = data.token; // Assuming the server returns the JWT in a 'token' field
-				// Store the token in localStorage or a cookie for future requests
-				localStorage.setItem('token', curr_token);
-				console.log(curr_token);
-				setToken(curr_token);
-				setIsAuthenticated(true);
+			const token = localStorage.getItem('token');
+
+			if (!token) {
+				const response = await fetch(BASE_URL + '/wp-json/jwt-auth/v1/token', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({ username, password })
+				});
+				console.log(response);
+				if (response.ok) {
+					const data = await response.json();
+					const curr_token = data.token; // Assuming the server returns the JWT in a 'token' field
+					// Store the token in localStorage or a cookie for future requests
+					localStorage.setItem('token', curr_token);
+					console.log(curr_token);
+					setToken(curr_token);
+					setIsAuthenticated(true);
+				} else {
+					// Handle authentication error
+					const errorData = await response.json();
+					const errorMessage = errorData.message; // Assuming the server returns an error message
+					console.error(errorMessage);
+					return false;
+				}
 			} else {
-				// Handle authentication error
-				const errorData = await response.json();
-				const errorMessage = errorData.message; // Assuming the server returns an error message
-				console.error(errorMessage);
-				return false;
+				console.log('already logged in');
 			}
 		},
 		logout: () => {
 			setIsAuthenticated(false);
+			setToken('');
+			localStorage.removeItem("token");
 		},
 	};
+
+	useEffect(
+		() => {
+			if (localStorage.getItem('token')) {
+				console.log(localStorage.getItem('token'));
+				setToken(localStorage.getItem('token'));
+				setIsAuthenticated(true);
+			}
+		},
+		[]
+	);
+
 
 	return (
 		<AuthContext.Provider value={auth}>
