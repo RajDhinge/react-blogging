7dedd23551007145f7b6b2d4b30b46d64cc96d0d
Raj Dhinge <rajkumar.dhinge.mca15@gmail.com>
2023-07-06 16:12:05 +0530

    :zap: fixed title update input

--- react-decoupled-wordpress/src/App.js
+++ react-decoupled-wordpress/src/App.js
@@ -11,13 +11,15 @@ import { BASE_URL, TINY_MCE_KEY } from './components/constants';
 
 function App() {
 	const editorRef = useRef(null);
+	const titleRef = useRef(null);
 	const auth = useAuth();
 
 	const log = () => {
-		if (editorRef.current) {
+		if (editorRef.current && titleRef.current) {
+			console.log("Saving data");
 			if (auth.isAuthenticated) {
 				const body = JSON.stringify({
-					title: 'My New Post',
+					title: titleRef.current.value,
 					content: editorRef.current.getContent()
 				});
 				auth.fetchData('http://react.local/wp-json/wp/v2/posts', 'POST', body);
@@ -55,7 +57,7 @@ function App() {
 						<br />
 						<div className="mb-6">
 							<label htmlFor="default-input" className="text-3xl md:text-4xl block mb-2 font-medium text-gray-900">Blog Title</label>
-							<input placeholder="Title" type="text" id="default-input" className="bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+							<input placeholder="Title" type="text" id="default-input" className="bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" ref={titleRef} />
 						</div>
 						<Editor
 							apiKey={TINY_MCE_KEY}
--- react-decoupled-wordpress/src/components/constants.js
+++ react-decoupled-wordpress/src/components/constants.js
@@ -1,2 +1,2 @@
 export const BASE_URL = "http://react.local";
-export const TINY_MCE_KEY = "";
+export const TINY_MCE_KEY = "jzd9j5dzpfs120fv2yj4q70a3hzg1a64sxei8105ufsxa6vm";
